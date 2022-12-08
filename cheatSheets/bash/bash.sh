744152d9b0790437decd0371350bef9eb2e81474
cypri <avicocyprien@yahoo.com>
2022-12-08 10:08:22 +0100

    css file add

---
+++ cheatSheets/bash/bash.sh
@@ -0,0 +1,17 @@
+
+################## for loop #######################
+## display only odd natural number from 1 to 99
+for ((i=1;i<=99;i=i+2)) ## (start value ; out condition ; increment rate)
+  do
+    echo "$i"; ## diplay only odd natural number
+  done
+
+## display only natural number from 1 to 50
+for ((i=1;i<=50;i=i+1))
+do
+    echo $i ## diplay natural number
+done
+
+################# display a var in parameter ######
+read var ## tape a variable var in stdin
+echo "Welcome" $var ## print the variable var in stdout
